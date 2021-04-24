5cb4899e72059752b2c5914593fcd1fc4cd29cbb
Lewis Whiteman <konomira@gmail.com>
2021-04-24 23:48:22 +0100

    Project cleanup

---
+++ Winding down/Assets/Player/PlayerMover.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        public float fallSpeed;
+        public Transform playerVisual;
+
+        private int pos = 1;
+        void Update()
+        {
+            transform.position += Vector3.down * (fallSpeed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (pos <= 0) return;
+
+                var oldPos = playerVisual.position;
+                pos--;
+                switch (pos)
+                {
+                    case 0:
+                        oldPos.x = 1.0f;
+                        break;
+                    case 1:
+                        oldPos.x = 0.3f;
+                        break;
+                }
+
+                playerVisual.position = oldPos;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (pos >= 2) return;
+
+                var oldPos = playerVisual.position;
+                pos++;
+
+                switch (pos)
+                {
+                    case 1:
+                        oldPos.x = 0.3f;
+                        break;
+                    case 2:
+                        oldPos.x = -0.4f;
+                        break;
+                }
+
+                playerVisual.position = oldPos;
+            }
+        }
+    }
+}
---
+++ Winding down/Assets/Steps/StepGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Steps
+{
+    public class StepGenerator : MonoBehaviour
+    {
+        public int stepCount;
+        public float stepHeight;
+
+        public float rotateSpeed;
+    
+    
+        public Transform root;
+    
+        public GameObject stepPrefab;
+    
+    
+    
+        private List<GameObject> steps = new List<GameObject>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (var i = 0; i < stepCount; i++)
+            {
+                var go = Instantiate(stepPrefab, root.position - Vector3.up * stepHeight * i, Quaternion.identity);
+                go.transform.parent = root;
+                go.transform.Rotate(Vector3.up, 10 * i);
+                steps.Add(go);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (var step in steps) 
+                step.transform.Rotate(Vector3.up, -rotateSpeed * Time.deltaTime);
+        }
+    }
+}
